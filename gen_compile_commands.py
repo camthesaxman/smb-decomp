b76a207c8ad8e8bca61d4541fd7df054be91c540
ComplexPlane <complexplanegames@gmail.com>
2022-02-22 18:37:04 -0500

    gen_compile_commands.py fixes
    - Export absolute paths, even on Linux
    - Add some defines so more C code is exposed for clangd
    - Remove extraneous spaces in output
    - Executable bit

--- gen_compile_commands.py
+++ gen_compile_commands.py
@@ -6,15 +6,18 @@ import json
 
 def main():
     cmds = []
-    script_dir = Path(__file__).parent
+    script_dir = Path(__file__).parent.resolve()
     for src_path in (script_dir / "src").iterdir():
         if not src_path.suffix == ".c":
             continue
 
-        command = f"/usr/bin/cc \
-            -I {script_dir} -I {script_dir / 'include'} \
-            -m32 -std=c99 -D __GNUC__ \
-            -c {src_path} -o {src_path.with_suffix('.o')}"
+        command = (
+            "/usr/bin/cc "
+            f"-I {script_dir} -I {script_dir / 'include'} "
+            "-m32 -std=c99 "
+            "-D __GNUC__ -D NONMATCHING -D MATHUTIL_C_ONLY "
+            f"-c {src_path} -o {src_path.with_suffix('.o')}"
+        )
         cmds.append(
             {
                 "directory": str(script_dir),
