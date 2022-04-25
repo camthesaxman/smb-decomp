c9e6c6eb4ea021508a02f44e71255f129432ddc0
ch-mcl <mphchmcl@gmail.com>
2022-04-25 18:26:59 +0900

    fix func_8009EA30 (parameters and local value change)

--- src/tevutil.h
+++ src/tevutil.h
@@ -27,7 +27,7 @@ void GXSetTevColorOp_cached(GXTevStageID stage, GXTevOp op, GXTevBias bias, GXTe
 void GXSetTevColorOp_cached_init(GXTevStageID stage, GXTevOp op, GXTevBias bias, GXTevScale scale, GXBool clamp, GXTevRegID out_reg);
 void GXSetTevAlphaOp_cached(GXTevStageID stage, GXTevOp op, GXTevBias bias, GXTevScale scale, u32 clamp, GXTevRegID out_reg);
 void GXSetTevAlphaOp_cached_init(GXTevStageID stage, GXTevOp op, GXTevBias bias, GXTevScale scale, GXBool clamp, GXTevRegID out_reg);
-void func_8009EA30(GXTevStageID stage, s32 gmaTevOp);
+void func_8009EA30(GXTevStageID stage, GXTevMode mode);
 void GXSetTevOrder_cached(GXTevStageID stage, GXTexCoordID coord, GXTexMapID map, GXChannelID color);
 void GXSetTevOrder_cached_init(GXTevStageID stage, GXTexCoordID coord, GXTexMapID map, GXChannelID color);
 void GXSetTevKColorSel_cached(GXTevStageID stage, GXTevKColorSel sel);
